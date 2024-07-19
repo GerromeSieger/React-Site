0643a26b0f1f14790eff9fcaac0b1be8c7ec4739
vsifah <vsifah@TeamCity>
2024-07-19 18:17:27 +0000

    TeamCity change in 'React Site' project: runners of 'React' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,6 +1,9 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.DockerCommandStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.buildSteps.sshExec
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -30,6 +33,78 @@ changeBuildType(RelativeId("Build")) {
         }
         add {
             password("SONAR_TOKEN", "credentialsJSON:617e93e3-e92c-4c35-94bd-b123bd30952e", display = ParameterDisplay.HIDDEN, readOnly = true)
+        }
+    }
+
+    expectSteps {
+        dockerCommand {
+            name = "test"
+            commandType = other {
+                subCommand = "run"
+                commandArgs = """
+                    --rm 
+                    -e SONAR_HOST_URL=%SONAR_HOST_URL%
+                    -e SONAR_LOGIN=sqa_6886e865b8e42c330891b056a3351385b8f85133
+                    -v "%teamcity.build.checkoutDir%:/usr/src" 
+                    sonarsource/sonar-scanner-cli:latest 
+                    -Dsonar.projectKey=%PROJECT_KEY%
+                """.trimIndent()
+            }
+        }
+        dockerCommand {
+            name = "Login to DockerHub"
+            commandType = other {
+                subCommand = "run"
+                commandArgs = """
+                    --rm 
+                    -e DOCKER_USERNAME="%env.DOCKERHUB_USERNAME%" 
+                    -e DOCKER_PASSWORD="%env.DOCKERHUB_PASSWORD%" 
+                    docker:dind 
+                    sh -c "echo ${'$'}DOCKER_PASSWORD | docker login -u ${'$'}DOCKER_USERNAME --password-stdin"
+                """.trimIndent()
+            }
+        }
+        dockerCommand {
+            name = "Build Docker Image"
+            commandType = build {
+                source = file {
+                    path = "Dockerfile"
+                }
+                contextDir = "."
+                namesAndTags = "%env.DOCKER_IMAGE%"
+            }
+        }
+        dockerCommand {
+            name = "Push Docker Image"
+            commandType = push {
+                namesAndTags = "%env.DOCKER_IMAGE%"
+            }
+        }
+        sshExec {
+            name = "Deploy to EC2 Instance"
+            commands = "docker run -p 5000:80 -d --name myapp %env.DOCKER_IMAGE%"
+            targetUrl = "%env.HOST%"
+            authMethod = uploadedKey {
+                username = "%env.USER%"
+                key = "id_rsa"
+            }
+        }
+    }
+    steps {
+        update<DockerCommandStep>(0) {
+            clearConditions()
+            commandType = other {
+                subCommand = "run"
+                workingDir = ""
+                commandArgs = """
+                    --rm 
+                    -e SONAR_HOST_URL=%SONAR_HOST_URL%
+                    -e SONAR_LOGIN="sqa_6886e865b8e42c330891b056a3351385b8f85133"
+                    -v "%teamcity.build.checkoutDir%:/usr/src" 
+                    sonarsource/sonar-scanner-cli:latest 
+                    -Dsonar.projectKey=%PROJECT_KEY%
+                """.trimIndent()
+            }
         }
     }
 }
