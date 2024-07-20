170d0bd4d3ce563d27f9c7d9fcc72cc9a0f5c57d
Victor <vsifah@gmail.com>
2024-07-20 13:28:56 +0100

    Update

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,28 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2024.03"
 
@@ -41,6 +20,47 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        nodeJS {
+            name = "build"
+            id = "nodejs_runner"
+            executionMode = BuildStep.ExecutionMode.ALWAYS
+            shellScript = """
+                npm install
+                npm run build
+            """.trimIndent()
+        }
+        script {
+            name = "test"
+            id = "simpleRunner"
+            scriptContent = "./test.sh"
+            dockerImage = "ubuntu:22.04"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+            dockerPull = true
+        }
+        sshUpload {
+            name = "deploy_copy"
+            id = "deploy_copy"
+            transportProtocol = SSHUpload.TransportProtocol.SCP
+            sourcePath = "build => ."
+            targetUrl = "%env.HOST%:/root/"
+            authMethod = uploadedKey {
+                username = "%env.USER%"
+                key = "id_rsa"
+            }
+        }
+        sshExec {
+            name = "deploy_run"
+            id = "deploy_run"
+            commands = "cp -r build/* /var/www/html && systemctl restart nginx"
+            targetUrl = "%env.HOST%"
+            authMethod = uploadedKey {
+                username = "%env.USER%"
+                key = "id_rsa"
+            }
+        }
     }
 
     triggers {
