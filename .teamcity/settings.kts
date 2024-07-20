09f3f7267e6693dbacdbb3955c344f0161ec868d
Victor <vsifah@gmail.com>
2024-07-20 14:18:42 +0100

    new

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -22,47 +22,6 @@ object Build : BuildType({
         root(DslContext.settingsRoot)
     }
 
-    steps {
-        nodeJS {
-            name = "build"
-            id = "nodejs_runner"
-            executionMode = BuildStep.ExecutionMode.ALWAYS
-            shellScript = """
-                npm install
-                npm run build
-            """.trimIndent()
-        }
-        script {
-            name = "test"
-            id = "simpleRunner"
-            scriptContent = "./test.sh"
-            dockerImage = "ubuntu:22.04"
-            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-            dockerPull = true
-        }
-        sshUpload {
-            name = "deploy_copy"
-            id = "deploy_copy"
-            transportProtocol = SSHUpload.TransportProtocol.SCP
-            sourcePath = "build => ."
-            targetUrl = "%env.HOST%:/root/"
-            authMethod = uploadedKey {
-                username = "%env.USER%"
-                key = "id_rsa"
-            }
-        }
-        sshExec {
-            name = "deploy_run"
-            id = "deploy_run"
-            commands = "cp -r build/* /var/www/html && systemctl restart nginx"
-            targetUrl = "%env.HOST%"
-            authMethod = uploadedKey {
-                username = "%env.USER%"
-                key = "id_rsa"
-            }
-        }
-    }
-
     triggers {
         vcs {
         }
