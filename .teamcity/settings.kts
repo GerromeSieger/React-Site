b8d4301fc47512ebaca7e039b2b27b529555a84b
Victor <vsifah@gmail.com>
2024-07-20 14:28:00 +0100

    new

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,10 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.SSHUpload
-import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
-import jetbrains.buildServer.configs.kotlin.buildSteps.nodeJS
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.buildSteps.sshExec
-import jetbrains.buildServer.configs.kotlin.buildSteps.sshUpload
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
@@ -25,47 +19,6 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
-    }
-
-    steps {
-        nodeJS {
-            name = "build"
-            id = "nodejs_runner"
-            executionMode = BuildStep.ExecutionMode.ALWAYS
-            shellScript = """
-                npm install
-                npm run build
-            """.trimIndent()
-        }
-        script {
-            name = "test"
-            id = "simpleRunner"
-            scriptContent = "./test.sh"
-            dockerImage = "ubuntu:22.04"
-            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-            dockerPull = true
-        }
-        sshUpload {
-            name = "deploy_copy"
-            id = "deploy_copy"
-            transportProtocol = SSHUpload.TransportProtocol.SCP
-            sourcePath = "build => ."
-            targetUrl = "%env.HOST%:/root/"
-            authMethod = uploadedKey {
-                username = "%env.USER%"
-                key = "id_rsa"
-            }
-        }
-        sshExec {
-            name = "deploy_run"
-            id = "deploy_run"
-            commands = "cp -r build/* /var/www/html && systemctl restart nginx"
-            targetUrl = "%env.HOST%"
-            authMethod = uploadedKey {
-                username = "%env.USER%"
-                key = "id_rsa"
-            }
-        }
     }
 
     triggers {
