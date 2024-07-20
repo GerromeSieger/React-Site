125e0e5c4462a428c60db09429e1800f46a32ae8
Gerrome <Gerrome@TeamCity>
2024-07-20 12:19:07 +0000

    TeamCity change in 'React Site' project: project parameters were changed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -31,6 +31,7 @@ project {
     buildType(Build)
 
     params {
+        password("env.USER", "credentialsJSON:12db73fa-8d7c-46b9-b2c6-eaa926b3eab5")
         password("env.HOST", "credentialsJSON:e960e252-27b0-42d3-9b47-4fe2bef751e9")
     }
 }
